d182513f604576b72cc6313928483c001593911a
GideonBrimleaf <chrisdryden1@gmail.com>
2020-11-04 13:44:17 +0000

    adds a marker to map

--- public/maps.js
+++ public/maps.js
@@ -1,11 +1,18 @@
-let map;
-
 function initMap() {
-  map = new google.maps.Map( document.getElementById( 'map' ), {
+  const map = new google.maps.Map( document.getElementById( 'map' ), {
     center: {
       lat: 51.513329,
       lng: -0.088950
     },
     zoom: 14
   });
+  
+  new google.maps.Marker({
+    position: {
+      lat: 51.513329,
+      lng: -0.088950
+    },
+    map,
+    title: `I'm Mary Poppins Y'All!`
+  })
 }

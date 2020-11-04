60171e084670d8303580296ee7968b24b9754e11
GideonBrimleaf <chrisdryden1@gmail.com>
2020-11-04 12:13:11 +0000

    refactors maps script to separate file

---
+++ public/maps.js
@@ -0,0 +1,11 @@
+let map;
+
+function initMap() {
+  map = new google.maps.Map( document.getElementById( 'map' ), {
+    center: {
+      lat: 51.513329,
+      lng: -0.088950
+    },
+    zoom: 14
+  });
+}

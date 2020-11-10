2cb672e1a63100d96e15c336dfad2d05a1cb55ed
GideonBrimleaf <chrisdryden1@gmail.com>
2020-11-10 15:44:14 +0000

    refactors component out of main script

---
+++ public/components/app.js
@@ -0,0 +1,18 @@
+export default {
+  name: 'App',
+  data() {
+    return {
+      stuff: ["Banana", "Orange", "Apple"],
+      selectedStuff: ''
+    }
+  },
+  template: `
+    <section>
+      <h1>My Amazing Selector!</h1>
+      <select v-model="selectedStuff">
+        <option value="" selected="selected" disabled>Select a Thing:</option>
+        <option v-for="thing in this.stuff">{{thing}}</option>
+      </select>
+    </section>
+  `,
+};
--- public/main.js
+++ public/main.js
@@ -1,21 +1,4 @@
-const App = {
-  name: 'App',
-  data() {
-    return {
-      stuff: ["Banana", "Orange", "Apple"],
-      selectedStuff: ''
-    }
-  },
-  template: `
-    <section>
-      <h1>My Amazing Selector!</h1>
-      <select v-model="selectedStuff">
-        <option value="" selected="selected" disabled>Select a Thing:</option>
-        <option v-for="thing in this.stuff">{{thing}}</option>
-      </select>
-    </section>
-  `,
-};
+import App from './components/app.js'
 
 new Vue({
   render: h => h(App),
